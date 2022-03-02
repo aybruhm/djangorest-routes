20c184cd6bfafb965c1e63448a486dd3c2443c0e
Israel Abraham <israelvictory87@gmail.com>
2022-03-02 15:05:55 +0100

    Setting up package bundling

--- setup.py
+++ setup.py
@@ -1,27 +1,29 @@
 import os
+from pathlib import Path
 from setuptools import setup
  
-README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+README=Path("README.rst").read_text(encoding="utf-8")
  
 # Allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
  
 setup(
-    name = 'djangorest-auth',
+    name = 'djangorest_auth',
     version = '0.1',
     packages = ['djangorest_auth'],
     include_package_data = True,
     license = 'BSD License',
     description = 'Bootstrap your spa startup with this amazing django rest authentication reusable app',
     long_description = README,
-    url = 'http://www.example.com/',
+    url = 'https://github.com/israelabraham/djangorest-auth',
     author = 'Abram 🐼',
     author_email = 'israelvictory87@gmail.com',
     classifiers =[
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
@@ -29,8 +31,17 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content'
-    ]
+    ],
+    install_requires=[
+        "django",
+        "djangorestframework",
+        "djangorestframework-simplejwt",
+        "rest-api-payload",
+        "django-environ",
+    ],
 )
 
