3b502d5954e32843f6e38284739e81d125763568
Israel Abraham <israelvictory87@gmail.com>
2022-03-08 16:29:57 +0100

    Update otp verifications logics

--- rest_auth/otp_verifications.py
+++ rest_auth/otp_verifications.py
@@ -14,65 +14,61 @@ class OTPVerification:
 
     def generate_otp_code(self, email):
 
-        # Get user by their unique username
+        """Get user email"""
         user = User.objects.get(email=email)
 
-        # Checks if the user exists
+        """Confirm if user exists"""
         if user:
 
-            # Generates an OTP code for the user
+            """Generates an OTP code for the user"""
             user_otp = "".join(choices(self.DIGITS * 2, k=6))
             user.otp_code = user_otp
 
-            # Saves OTP code to user table
+            """Saves otp code"""
             user.save()
             otp_code = user.otp_code
             return otp_code
 
     def send_otp_code_to_email(self, email, first_name=None):
+        """Generate user otp code"""
         otp_code = self.generate_otp_code(email=email)
         
-        # ------------------------------------------
-        # Gets user email
-        # ------------------------------------------
+        """Gets current email making request"""
         user = User.objects.get(email=email)
         
-        # Email context
+        """context for email template"""
         context = {
-            "firstname": user.firstname,
+            "firstname": None if user.firstname is None else "",
             "otp_code": user.otp_code.id
         }
         
-        # ------------------------------------------
-        # Send Welcome HTML Email Template to user
-        # ------------------------------------------
+        """Send otp html email to user"""
         send_html_to_email(
-            to_list=[user.email], subject="VERIFY YOUR ACCOUNT",
-            template_name="emails/users/verify.html", 
+            to_list=[user.email], 
+            subject="Django Rest Auth - VERIFY YOUR ACCOUNT",
+            template_name="emails/authentication/otp_verify.html", 
             context=context,
         )
-        
         return otp_code
 
     def verify_otp_code_from_email(self, otp_code, email):
 
-        # Get user by their unique email
+        """Get user email"""
         user = User.objects.get(email=email)
 
-        # If the coming otp_code matches the user otp_code id, verify
+        """Check if otp code matches with the user otp code hash id"""
         if int(otp_code) == user.otp_code.id:
             user.is_active = True
             user.is_email_active = True
             user.save()
-            
             return True
 
-    def destroy_otp_code(self, username):
+    def destroy_otp_code(self, email:str):
 
-        # Get user by their unique username
-        user = User.objects.get(username=username)
+        """Get user email"""
+        user = User.objects.get(email=email)
 
-        # Destroys otp code
+        """Destroys user otp code"""
         user.otp_code = ""
         user.save()
         otp_code = user.otp_code
