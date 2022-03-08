30db119a36b0b2d2d3b0fc1a55014231d72cc485
Israel Abraham <israelvictory87@gmail.com>
2022-03-08 15:52:34 +0100

    Created custom User model

--- rest_auth/models.py
+++ rest_auth/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import (
+    AbstractBaseUser, PermissionsMixin
+)
 from django.db import models
+from rest_auth.managers import UserManager
 
-# Create your models here.
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    id = models.BigAutoField(auto_created=True, primary_key=True)
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=11, unique=True, default=0)
+    email = models.EmailField(db_index=True, unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['firstname', 'lastname', 'phone_number']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
+
+    class Meta:
+        verbose_name_plural = "Users"
