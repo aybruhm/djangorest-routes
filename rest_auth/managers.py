bbf4c5b235b910b69f6f3f0bc4d37de8547450dc
Israel Abraham <israelvictory87@gmail.com>
2022-03-08 15:52:23 +0100

    Created customo User Manager

---
+++ rest_auth/managers.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, firstname:str, lastname:str, email:str, phone_number:str, password=None):
+
+        if email is None:
+            raise TypeError('Users must have an email address')
+
+        if phone_number is None:
+            raise TypeError("User must have a phone number")
+
+        user = self.model(
+            firstname=firstname, lastname=lastname,
+            email=self.normalize_email(email), phone_number=phone_number
+        )
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, firstname, lastname, phone_number, email, password):
+        if firstname is None:
+            raise TypeError('Users must have a Firstname')
+
+        if email is None:
+            raise TypeError('Users must have an email address')
+
+        if phone_number is None:
+            raise TypeError("User must have a phone number")
+
+        if password is None:
+            raise TypeError('Superusers must have a password.')
+
+        user = self.create_user(firstname=firstname, lastname=lastname, email=email, phone_number=phone_number, password=password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
