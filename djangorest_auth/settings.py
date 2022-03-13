3ad192b2becef514a84fb1663fa40b29bbaf6d13
Israel Abraham <israelvictory87@gmail.com>
2022-03-13 10:23:16 +0100

    Update settings.py

--- djangorest_auth/settings.py
+++ djangorest_auth/settings.py
@@ -113,9 +113,9 @@ SIMPLE_JWT = {
 
 CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3030',
-] # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:3030',
+# ] # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
 
 
 ROOT_URLCONF = 'djangorest_auth.urls'
