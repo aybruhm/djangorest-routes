0f2243728f1d12da8ec67e4350227f20255cc988
Israel Abraham <israelvictory87@gmail.com>
2022-03-13 16:04:05 +0100

    Added Hash field salt

--- djangorest_auth/config/local.py
+++ djangorest_auth/config/local.py
@@ -3,6 +3,11 @@ from djangorest_auth.config.base import *
 
 DEBUG = env("DEBUG")
 
+
+# HASH_FIELD_SALT WARNING: keep the salt key used in production secret!
+HASH_FIELD_SALT = "k^8p7869&jh9w7jhtnjun!di55o$rg0*g%mdi(_-3xue#)e=(!"
+
+
 ALLOWED_HOSTS += [
     "127.0.0.1", "localhost"
 ]
