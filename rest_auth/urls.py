da47013e8d395ede6a5cb19591b6a7c500931f78
Israel Abraham <israelvictory87@gmail.com>
2022-03-14 08:58:43 +0100

    Update rest_auth/urls.py

--- rest_auth/urls.py
+++ rest_auth/urls.py
@@ -1,11 +1,9 @@
 from rest_auth.views import (
     RegisterOniichan, ConfirmOniichanOTP,
-    ResendOniichanOTP,
+    ResendOniichanOTP, LoginOniichan,
+    RefreshLoginOniichan,
     ChangeOniichanPassword, Konnichiwa,
     LogOniichan, email_otp_verify
-)
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView, TokenRefreshView
 )
 from django.urls import path
 
@@ -14,10 +12,10 @@ app_name = 'authentication'
 urlpatterns = [
     path("", Konnichiwa.as_view(), name="welcome"),
     path("register/", RegisterOniichan.as_view(), name="register_user"),
+    path("login/", LoginOniichan.as_view(), name="login"),
+    path("login/refresh/", RefreshLoginOniichan.as_view(), name="login_refresh"),
     path("confirm_otp/", ConfirmOniichanOTP.as_view(), name="confirm_otp"),
     path("resend_otp_code/", ResendOniichanOTP.as_view(), name="resend_otp_code"),
-    path("login/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("login/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     # path("suspend_user/<str:email>/", SuspendUserApiView.as_view(), name="suspend_user"), 
     path("change_password/<str:email>/", ChangeOniichanPassword.as_view(), name="change_user_password"),
     path("logout/", LogOniichan.as_view(), name="logout_user"),
