d0ba925cb65dc2e426e6f56cc68428d91f65895c
Israel Abraham <israelvictory87@gmail.com>
2022-03-17 16:43:49 +0100

    Add field username to user table

--- rest_auth/models.py
+++ rest_auth/models.py
@@ -12,6 +12,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     id = models.BigAutoField(auto_created=True, primary_key=True)
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
+    username = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=11, unique=True, default=0)
     email = models.EmailField(db_index=True, unique=True)
     otp_code = HashidField(salt=HASH_FIELD_SALT, min_length=6, unique=True, blank=True, null=True)
@@ -19,7 +20,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_email_active = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['firstname', 'lastname', 'phone_number']
+    REQUIRED_FIELDS = ['firstname', 'lastname', 'phone_number', 'username']
 
     objects = UserManager()
 
