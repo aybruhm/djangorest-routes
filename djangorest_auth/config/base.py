2841f707f13aba68d36823f1c0c6787ff8b45a1f
Israel Abraham <israelvictory87@gmail.com>
2022-03-24 09:25:56 +0100

    Update base.py

--- djangorest_auth/config/base.py
+++ djangorest_auth/config/base.py
@@ -22,7 +22,7 @@ env = environ.Env(
 )
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Runtime Environment
