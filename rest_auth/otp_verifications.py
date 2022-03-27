86b60007f54bca7903b35448a7f44a1fc043052d
Israel Abraham <israelvictory87@gmail.com>
2022-03-27 22:03:44 +0100

    Documented otp verifications logics

--- rest_auth/otp_verifications.py
+++ rest_auth/otp_verifications.py
@@ -13,9 +13,14 @@ class OTPVerification:
     EXPIRE_TIME = 300
 
     def generate_otp_code(self, user_email:str):
+        """
+        Generates an OTP code for the user
         
-        print("User: ", User.objects.all())
-
+        :param user_email: The email of the user who wants to generate an OTP code
+        :type user_email: str
+        :return: The otp_code is being returned.
+        """
+        
         """Get user email"""
         user = User.objects.get(email=user_email)
 
@@ -32,10 +37,18 @@ class OTPVerification:
             return otp_code
 
     def send_otp_code_to_email(self, email:str, first_name:str):
+        """
+        Generate user otp code and send otp html email to user
+        
+        :param email: The email address of the user that needs to be verified
+        :type email: str
+        :param first_name: The first name of the user
+        :type first_name: str
+        :return: The otp_code object.
+        """
+        
         """Generate user otp code"""
         otp_code = self.generate_otp_code(user_email=email)
-        
-
         
         """Gets current email making request"""
         user = User.objects.get(email=email)
@@ -56,6 +69,13 @@ class OTPVerification:
         return otp_code
     
     def send_password_reset_otp_code_to_email(self, email:str):
+        """
+        Generate an OTP code for password reset and send it to the user's email
+        
+        :param email: The email address of the user who wants to reset their password
+        :type email: str
+        :return: The OTP code object.
+        """
             
         """Generate otp code for password reset"""
         otp_code = self.generate_otp_code(email=email)
@@ -75,6 +95,15 @@ class OTPVerification:
         return otp_code
 
     def verify_otp_code_from_email(self, otp_code:str, email:str):
+        """
+        Verify the otp code sent to the user's email address
+        
+        :param otp_code: The OTP code that you want to verify
+        :type otp_code: str
+        :param email: The email address of the user
+        :type email: str
+        :return: Nothing.
+        """
 
         """Get user email"""
         user = User.objects.get(email=email)
@@ -90,6 +119,13 @@ class OTPVerification:
             return True
 
     def destroy_otp_code(self, email:str):
+        """
+        This function destroys the otp code of the user
+        
+        :param email: The email of the user you want to get the otp code for
+        :type email: str
+        :return: The otp_code is being returned.
+        """
 
         """Get user email"""
         user = User.objects.get(email=email)
