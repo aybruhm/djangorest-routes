a596c842a3c4aa245c99a7ad5c0efe2071d54e31
Israel Abraham <israelvictory87@gmail.com>
2022-03-29 22:01:11 +0100

    Update __str__ method

--- rest_auth/models.py
+++ rest_auth/models.py
@@ -25,7 +25,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     def __str__(self):
-        return f"{self.firstname} {self.lastname}"
+        return self.email
 
     class Meta:
         verbose_name_plural = "Users"
