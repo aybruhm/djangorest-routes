75bf7cdca576f266b754e2bc8b0f7aa95fa00b34
Israel Abraham <israelvictory87@gmail.com>
2022-04-03 20:47:17 +0100

    Update UserAdmin

--- rest_routes/admin.py
+++ rest_routes/admin.py
@@ -5,5 +5,5 @@ from rest_routes.models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("id", "first_name", "last_name", "email", "username")
+    list_display = ("id", "firstname", "lastname", "email", "username")
     list_filter = ("id", "email", "username")
