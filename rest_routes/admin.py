c550bb0f4577e175aeb5914a8f25ac9be90fb5ba
Israel Abraham <israelvictory87@gmail.com>
2022-04-03 20:39:39 +0100

    Created UserAdmin

--- rest_routes/admin.py
+++ rest_routes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from rest_routes.models import User
 
-# Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "first_name", "last_name", "email", "username")
+    list_filter = ("id", "email", "username")
