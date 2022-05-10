1cec96860ee95775f30f6fff407a8a00f996a249
Israel Abraham <israelvictory87@gmail.com>
2022-05-10 22:29:27 +0100

    Update has_object_permission

--- rest_routes/permissions.py
+++ rest_routes/permissions.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import permission_required, login_required
 from django.http import HttpRequest
+from rest_framework.permissions import BasePermission
 
 
 @login_required
@@ -16,3 +17,14 @@ def suspend_user(request:HttpRequest):
         # Suspend user logic
         return True
     return False
+
+
+class SuspendUserPerm(BasePermission):
+    
+    def has_permission(self, request, view):
+        pass
+    
+    def has_object_permission(self, request, view, obj):
+        if obj.has_perm("user.can_suspend_user"):
+            
+            pass
